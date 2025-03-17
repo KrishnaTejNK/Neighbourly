d9d5eea180a1028d29b7b5546f0bc0d8666fa775
keshvi00 <keshvigodhani@gmail.com>
2025-03-17 00:06:37 -0300

    user story #10. Backend of Amenity

--- Backend/src/main/java/com/dalhousie/Neighbourly/amenity/dto/BookingRequestDTO.java
+++ Backend/src/main/java/com/dalhousie/Neighbourly/amenity/dto/BookingRequestDTO.java
@@ -1,2 +1,20 @@
-package com.dalhousie.Neighbourly.amenity.dto;public class BookingRequestDTO {
+package com.dalhousie.Neighbourly.amenity.dto;
+
+import com.dalhousie.Neighbourly.booking.entity.BookingRequest;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BookingRequestDTO {
+    private int bookingId;
+    private Integer neighbourhood_id;
+    private int user_id;
+    private int amenityId;
+    private String name;
+    private String description;
+    private LocalDateTime bookingFrom;
+    private LocalDateTime bookingTo;
+    private int expectedAttendees;
+    private BookingRequest.BookingStatus status = BookingRequest.BookingStatus.PENDING;
 }
